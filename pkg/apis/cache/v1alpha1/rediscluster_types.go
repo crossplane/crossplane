5d6b27c82ebcb235cd6d1ee02e9ab91437950946
Nic Cope <negz@rk0n.org>
2019-01-29 14:00:24 -0800

    Add autogenerated RedisCluster resources.
    This commit was autogenerated by running:
    
    ```bash
    $ kubebuilder create --group cache --version v1alpha1 --kind RedisCluster --controller=false --resource
    $ make codegen
    ```
    
    Signed-off-by: Nic Cope <negz@rk0n.org>

---
+++ pkg/apis/cache/v1alpha1/rediscluster_types.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2018 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// RedisClusterSpec defines the desired state of RedisCluster
+type RedisClusterSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// RedisClusterStatus defines the observed state of RedisCluster
+type RedisClusterStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// RedisCluster is the Schema for the redisclusters API
+// +k8s:openapi-gen=true
+type RedisCluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   RedisClusterSpec   `json:"spec,omitempty"`
+	Status RedisClusterStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// RedisClusterList contains a list of RedisCluster
+type RedisClusterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []RedisCluster `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&RedisCluster{}, &RedisClusterList{})
+}
