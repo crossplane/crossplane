417a66f5a58b659c9323b3b655b4231488e6f236
Nic Cope <negz@rk0n.org>
2019-01-30 19:11:08 -0800

    Add CloudMemoryStoreInstance CRD
    I started with a copy of the CloudSQLInstance boilerplate here rather than using
    kubebuilder, as our patterns seem to have diverged a little.
    
    It's worth noting I diverged from gomega in some tests in favour of a stdlib
    based table driven test (https://github.com/golang/go/wiki/TableDrivenTests)
    approach. Consider this an RFC.
    
    Signed-off-by: Nic Cope <negz@rk0n.org>

---
+++ pkg/apis/gcp/cache/v1alpha1/cloudmemorystore_instance_types.go
@@ -0,0 +1,243 @@
+/*
+Copyright 2018 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"strconv"
+	"strings"
+
+	redis "google.golang.org/genproto/googleapis/cloud/redis/v1"
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	corev1alpha1 "github.com/crossplaneio/crossplane/pkg/apis/core/v1alpha1"
+	"github.com/crossplaneio/crossplane/pkg/util"
+)
+
+// Cloud Memorystore instance states.
+var (
+	StateUnspecified = redis.Instance_STATE_UNSPECIFIED.String()
+	StateCreating    = redis.Instance_CREATING.String()
+	StateReady       = redis.Instance_READY.String()
+	StateUpdating    = redis.Instance_UPDATING.String()
+	StateDeleting    = redis.Instance_DELETING.String()
+	StateRepairing   = redis.Instance_REPAIRING.String()
+	StateMaintenance = redis.Instance_MAINTENANCE.String()
+)
+
+// Cloud Memorystore instance tiers.
+var (
+	TierBasic      = redis.Instance_BASIC.String()
+	TierStandardHA = redis.Instance_STANDARD_HA.String()
+)
+
+// CloudMemorystoreInstanceSpec defines the desired state of CloudMemorystoreInstance
+// Most fields map directly to a GCP Instance resource.
+// https://cloud.google.com/memorystore/docs/redis/reference/rest/v1/projects.locations.instances#Instance
+type CloudMemorystoreInstanceSpec struct {
+	// Region in which to create this CloudMemorystore cluster.
+	Region string `json:"region"`
+
+	// Tier specifies the replication level of the Redis cluster. BASIC provides
+	// a single Redis instance with no high availability. STANDARD_HA provides a
+	// cluster of two Redis instances in distinct availability zones.
+	// https://cloud.google.com/memorystore/docs/redis/redis-tiers
+	// +kubebuilder:validation:Enum=BASIC,STANDARD_HA
+	Tier string `json:"tier"`
+
+	// LocationID specifies the zone where the instance will be provisioned. If
+	// not provided, the service will choose a zone for the instance. For
+	// STANDARD_HA tier, instances will be created across two zones for
+	// protection against zonal failures.
+	LocationID string `json:"locationId,omitempty"`
+
+	// AlternativeLocationID is only applicable to STANDARD_HA tier, which
+	// protects the instance against zonal failures by provisioning it across
+	// two zones. If provided, it must be a different zone from the one provided
+	// in locationId.
+	AlternativeLocationID string `json:"alternativeLocationId,omitempty"`
+
+	// MemorySizeGB specifies the Redis memory size in GiB.
+	MemorySizeGB int `json:"memorySizeGb"`
+
+	// ReservedIPRange specifies the CIDR range of internal addresses that are
+	// reserved for this instance. If not provided, the service will choose an
+	// unused /29 block, for example, 10.0.0.0/29 or 192.168.0.0/29. Ranges must
+	// be unique and non-overlapping with existing subnets in an authorized
+	// network.
+	ReservedIPRange string `json:"reservedIpRange,omitempty"`
+
+	// AuthorizedNetwork specifies the full name of the Google Compute Engine
+	// network to which the instance is connected. If left unspecified, the
+	// default network will be used.
+	AuthorizedNetwork string `json:"authorizedNetwork,omitempty"`
+
+	// RedisVersion specifies the version of Redis software. If not provided,
+	// latest supported version will be used. Updating the version will perform
+	// an upgrade/downgrade to the new version. Currently, the supported values
+	// are REDIS_3_2 for Redis 3.2.
+	// +kubebuilder:validation:Enum=REDIS_3_2
+	RedisVersion string `json:"redisVersion,omitempty"`
+
+	// RedisConfigs specifies Redis configuration parameters, according to
+	// http://redis.io/topics/config. Currently, the only supported parameters
+	// are:
+	// * maxmemory-policy
+	// * notify-keyspace-events
+	RedisConfigs map[string]string `json:"redisConfigs,omitempty"`
+
+	// Kubernetes object references
+	ClaimRef            *v1.ObjectReference     `json:"claimRef,omitempty"`
+	ClassRef            *v1.ObjectReference     `json:"classRef,omitempty"`
+	ProviderRef         v1.LocalObjectReference `json:"providerRef"`
+	ConnectionSecretRef v1.LocalObjectReference `json:"connectionSecretRef,omitempty"`
+
+	// ReclaimPolicy identifies how to handle the cloud resource after the deletion of this type
+	ReclaimPolicy corev1alpha1.ReclaimPolicy `json:"reclaimPolicy,omitempty"`
+}
+
+// CloudMemorystoreInstanceStatus defines the observed state of CloudMemorystoreInstance
+type CloudMemorystoreInstanceStatus struct {
+	corev1alpha1.ConditionedStatus
+	corev1alpha1.BindingStatusPhase
+	State   string `json:"state,omitempty"`
+	Message string `json:"message,omitempty"`
+
+	// ProviderID is the external ID to identify this resource in the cloud
+	// provider
+	ProviderID string `json:"providerID,omitempty"`
+
+	// CurrentLocationID is the current zone where the Redis endpoint is placed.
+	// For Basic Tier instances, this will always be the same as the locationId
+	// provided by the user at creation time. For Standard Tier instances, this
+	// can be either locationId or alternativeLocationId and can change after a
+	// failover event.
+	CurrentLocationID string `json:"currentLocationId,omitempty"`
+
+	// Endpoint of the Cloud Memorystore instance used in connection strings.
+	Endpoint string `json:"endpoint,omitempty"`
+
+	// Port at which the Cloud Memorystore instance endpoint is listening.
+	Port int `json:"port,omitempty"`
+
+	// InstanceName of the Cloud Memorystore instance. Does not include the
+	// project and location (region) IDs. e.g. 'foo', not
+	// 'projects/fooproj/locations/us-foo1/instances/foo'
+	InstanceName string `json:"instanceName,omitempty"`
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CloudMemorystoreInstance is the Schema for the instances API
+// +k8s:openapi-gen=true
+// +groupName=cache.gcp
+// +kubebuilder:printcolumn:name="STATUS",type="string",JSONPath=".status.state"
+// +kubebuilder:printcolumn:name="CLASS",type="string",JSONPath=".spec.classRef.name"
+// +kubebuilder:printcolumn:name="VERSION",type="string",JSONPath=".spec.redisVersion"
+// +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
+type CloudMemorystoreInstance struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CloudMemorystoreInstanceSpec   `json:"spec,omitempty"`
+	Status CloudMemorystoreInstanceStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CloudMemorystoreInstanceList contains a list of CloudMemorystoreInstance
+type CloudMemorystoreInstanceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []CloudMemorystoreInstance `json:"items"`
+}
+
+// NewCloudMemorystoreInstanceSpec creates a new CloudMemorystoreInstanceSpec
+// from the given properties map.
+func NewCloudMemorystoreInstanceSpec(properties map[string]string) *CloudMemorystoreInstanceSpec {
+	spec := &CloudMemorystoreInstanceSpec{
+		ReclaimPolicy: corev1alpha1.ReclaimRetain,
+
+		// Note that these keys should match the JSON tags of their respective
+		// CloudMemorystoreInstanceSpec fields.
+		Region:                properties["region"],
+		Tier:                  properties["tier"],
+		LocationID:            properties["locationId"],
+		AlternativeLocationID: properties["alternativeLocationId"],
+		ReservedIPRange:       properties["reservedIpRange"],
+		AuthorizedNetwork:     properties["authorizedNetwork"],
+		RedisVersion:          properties["redisVersion"],
+		RedisConfigs:          parseMap(properties["redisConfigs"]),
+	}
+
+	if i, err := strconv.Atoi(properties["memorySizeGb"]); err == nil {
+		spec.MemorySizeGB = i
+	}
+
+	return spec
+}
+
+// parseMap parses a string of comma separated key value pairs, for example
+// "key1: value1, key2: value2", into a string map.
+func parseMap(s string) map[string]string {
+	m := map[string]string{}
+	for _, cfg := range strings.Split(s, ",") {
+		if kv := strings.SplitN(cfg, ":", 2); len(kv) == 2 {
+			m[strings.TrimSpace(kv[0])] = strings.TrimSpace(kv[1])
+		}
+	}
+	return m
+}
+
+// ConnectionSecretName returns a secret name from the reference
+func (c *CloudMemorystoreInstance) ConnectionSecretName() string {
+	if c.Spec.ConnectionSecretRef.Name == "" {
+		c.Spec.ConnectionSecretRef.Name = c.Name
+	}
+
+	return c.Spec.ConnectionSecretRef.Name
+}
+
+// ObjectReference to this CloudMemorystore instance
+func (c *CloudMemorystoreInstance) ObjectReference() *v1.ObjectReference {
+	return util.ObjectReference(c.ObjectMeta, util.IfEmptyString(c.APIVersion, APIVersion), util.IfEmptyString(c.Kind, CloudMemorystoreInstanceKind))
+}
+
+// OwnerReference to use this instance as an owner
+func (c *CloudMemorystoreInstance) OwnerReference() metav1.OwnerReference {
+	return *util.ObjectToOwnerReference(c.ObjectReference())
+}
+
+// IsAvailable for usage/binding
+func (c *CloudMemorystoreInstance) IsAvailable() bool {
+	return c.Status.State == StateReady
+}
+
+// IsBound determines if the resource is in a bound binding state
+func (c *CloudMemorystoreInstance) IsBound() bool {
+	return c.Status.Phase == corev1alpha1.BindingStateBound
+}
+
+// SetBound sets the binding state of this resource
+func (c *CloudMemorystoreInstance) SetBound(state bool) {
+	if state {
+		c.Status.Phase = corev1alpha1.BindingStateBound
+	} else {
+		c.Status.Phase = corev1alpha1.BindingStateUnbound
+	}
+}
---
+++ pkg/apis/gcp/cache/v1alpha1/cloudmemorystore_instance_types_test.go
@@ -0,0 +1,177 @@
+/*
+Copyright 2018 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"context"
+	"log"
+	"testing"
+
+	"github.com/go-test/deep"
+	"github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	corev1alpha1 "github.com/crossplaneio/crossplane/pkg/apis/core/v1alpha1"
+	"github.com/crossplaneio/crossplane/pkg/test"
+)
+
+const (
+	namespace = "default"
+	name      = "test-instance"
+)
+
+var (
+	cfg *rest.Config
+	c   client.Client
+	ctx = context.TODO()
+)
+
+func TestMain(m *testing.M) {
+	err := SchemeBuilder.AddToScheme(scheme.Scheme)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t := test.NewTestEnv(namespace, test.CRDs())
+	cfg = t.Start()
+
+	if c, err = client.New(cfg, client.Options{Scheme: scheme.Scheme}); err != nil {
+		log.Fatal(err)
+	}
+
+	t.StopAndExit(m.Run())
+}
+
+func TestStorageCloudMemorystoreInstance(t *testing.T) {
+	key := types.NamespacedName{Name: name, Namespace: namespace}
+	created := &CloudMemorystoreInstance{
+		ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: namespace},
+		Spec:       CloudMemorystoreInstanceSpec{Tier: TierBasic},
+	}
+	g := gomega.NewGomegaWithT(t)
+
+	// Test Create
+	g.Expect(c.Create(ctx, created)).NotTo(gomega.HaveOccurred())
+
+	fetched := &CloudMemorystoreInstance{}
+	g.Expect(c.Get(ctx, key, fetched)).NotTo(gomega.HaveOccurred())
+	g.Expect(fetched).To(gomega.Equal(created))
+
+	// Test Updating the Labels
+	updated := fetched.DeepCopy()
+	updated.Labels = map[string]string{"hello": "world"}
+	g.Expect(c.Update(ctx, updated)).NotTo(gomega.HaveOccurred())
+
+	g.Expect(c.Get(ctx, key, fetched)).NotTo(gomega.HaveOccurred())
+	g.Expect(fetched).To(gomega.Equal(updated))
+
+	// Test Delete
+	g.Expect(c.Delete(ctx, fetched)).NotTo(gomega.HaveOccurred())
+	g.Expect(c.Get(ctx, key, fetched)).To(gomega.HaveOccurred())
+}
+
+func TestNewCloudMemorystoreInstanceSpec(t *testing.T) {
+	cases := []struct {
+		name       string
+		properties map[string]string
+		want       *CloudMemorystoreInstanceSpec
+	}{
+		{
+			name: "AllProperties",
+			properties: map[string]string{
+				"tier":                  TierBasic,
+				"region":                "au-east1",
+				"locationId":            "au-east1-a",
+				"alternativeLocationId": "au-east1-b",
+				"reservedIpRange":       "172.16.0.0/29",
+				"authorizedNetwork":     "default",
+				"memorySizeGb":          "4",
+				"redisVersion":          "REDIS_3_2",
+				"redisConfigs":          "max-memory-policy: lots, notify-keyspace-events: surewhynot",
+			},
+			want: &CloudMemorystoreInstanceSpec{
+				ReclaimPolicy:         corev1alpha1.ReclaimRetain,
+				Tier:                  TierBasic,
+				Region:                "au-east1",
+				LocationID:            "au-east1-a",
+				AlternativeLocationID: "au-east1-b",
+				ReservedIPRange:       "172.16.0.0/29",
+				AuthorizedNetwork:     "default",
+				MemorySizeGB:          4,
+				RedisVersion:          "REDIS_3_2",
+				RedisConfigs: map[string]string{
+					"max-memory-policy":      "lots",
+					"notify-keyspace-events": "surewhynot",
+				},
+			},
+		},
+		{
+			name:       "NilProperties",
+			properties: nil,
+			want: &CloudMemorystoreInstanceSpec{
+				ReclaimPolicy: corev1alpha1.ReclaimRetain,
+				RedisConfigs:  map[string]string{},
+			},
+		},
+		{
+			name:       "UnknownProperties",
+			properties: map[string]string{"unknown": "wat"},
+			want: &CloudMemorystoreInstanceSpec{
+				ReclaimPolicy: corev1alpha1.ReclaimRetain,
+				RedisConfigs:  map[string]string{},
+			},
+		},
+		{
+			name:       "MemorySizeGbNotANumber",
+			properties: map[string]string{"memorySizeGb": "wat"},
+			want: &CloudMemorystoreInstanceSpec{
+				ReclaimPolicy: corev1alpha1.ReclaimRetain,
+				RedisConfigs:  map[string]string{},
+				MemorySizeGB:  0,
+			},
+		},
+		{
+			name:       "RedisConfigsUnparseable",
+			properties: map[string]string{"redisConfigs": "wat,wat"},
+			want: &CloudMemorystoreInstanceSpec{
+				ReclaimPolicy: corev1alpha1.ReclaimRetain,
+				RedisConfigs:  map[string]string{},
+			},
+		},
+		{
+			name:       "RedisConfigsExtraneousWhitespace",
+			properties: map[string]string{"redisConfigs": "   verykey:suchvalue"},
+			want: &CloudMemorystoreInstanceSpec{
+				ReclaimPolicy: corev1alpha1.ReclaimRetain,
+				RedisConfigs:  map[string]string{"verykey": "suchvalue"},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := NewCloudMemorystoreInstanceSpec(tc.properties)
+			if diff := deep.Equal(got, tc.want); diff != nil {
+				t.Errorf("got != want:\n%v", diff)
+			}
+		})
+	}
+}
