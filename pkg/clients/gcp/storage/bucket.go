1b488ed40c4aff44a09f96fe1c68c6a83cb1c4cc
Illya Chekrygin <illya.chekrygin@gmail.com>
2019-03-26 12:52:56 -0700

    Add gcp storage client interface, base and mock implementations
    Signed-off-by: Illya Chekrygin <illya.chekrygin@gmail.com>

---
+++ pkg/clients/gcp/storage/bucket.go
@@ -0,0 +1,22 @@
+package storage
+
+import (
+	"context"
+
+	"cloud.google.com/go/storage"
+)
+
+// Client bucket resource operations interface
+type Client interface {
+	Attrs(context.Context) (*storage.BucketAttrs, error)
+	Create(context.Context, string, *storage.BucketAttrs) error
+	Update(context.Context, storage.BucketAttrsToUpdate) (*storage.BucketAttrs, error)
+	Delete(context.Context) error
+}
+
+// BucketClient implements Client interface
+type BucketClient struct {
+	*storage.BucketHandle
+}
+
+var _ Client = &BucketClient{}
