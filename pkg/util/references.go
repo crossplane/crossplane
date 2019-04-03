1ee7b77e01f00a8a3fc7c1549221fef793592850
Illya Chekrygin <illya.chekrygin@gmail.com>
2019-04-02 22:47:37 -0700

    Add a utility helper to add unique owner references to the object metadata
    Signed-off-by: Illya Chekrygin <illya.chekrygin@gmail.com>

--- pkg/util/references.go
+++ pkg/util/references.go
@@ -17,14 +17,35 @@ limitations under the License.
 package util
 
 import (
-	v1 "k8s.io/api/core/v1"
+	"reflect"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
 
 // NamespaceNameFromObjectRef helper function to create NamespacedName
-func NamespaceNameFromObjectRef(or *v1.ObjectReference) types.NamespacedName {
+func NamespaceNameFromObjectRef(or *corev1.ObjectReference) types.NamespacedName {
 	return types.NamespacedName{
 		Namespace: or.Namespace,
 		Name:      or.Name,
 	}
+}
+
+// AddOwnerReference to the object metadata, only if this owner reference
+// is not in the existing owner references list
+func AddOwnerReference(om *metav1.ObjectMeta, or metav1.OwnerReference) {
+	if om == nil {
+		return
+	}
+	if om.OwnerReferences == nil {
+		om.OwnerReferences = []metav1.OwnerReference{or}
+		return
+	}
+	for _, v := range om.OwnerReferences {
+		if reflect.DeepEqual(v, or) {
+			return
+		}
+	}
+	om.OwnerReferences = append(om.OwnerReferences, or)
 }
