db14ab6a58a60dc584a9183a79c49d0f1739b97d
Nic Cope <negz@rk0n.org>
2019-06-17 17:58:59 -0700

    Remove 'Boilerplate only' comment.
    This may have been true when these files were generated, but they've diverged
    enough that probably should not be recommending people ignore them.
    
    Signed-off-by: Nic Cope <negz@rk0n.org>

--- pkg/apis/aws/v1alpha1/register.go
+++ pkg/apis/aws/v1alpha1/register.go
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// NOTE: Boilerplate only.  Ignore this file.
 
 // Package v1alpha1 contains API Schema definitions for the aws v1alpha1 API group
 // +k8s:openapi-gen=true
