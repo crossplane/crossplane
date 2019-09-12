8aae32ea07b195bb9bb9701f7163f7250f9f59ad
soorena776 <javad@upbound.io>
2019-09-12 00:30:34 -0700

    adding managed resources, for resource connectivity
    Signed-off-by: soorena776 <javad@upbound.io>

---
+++ cluster/examples/aws-resource-connectivity-setup/build_provider.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+#
+# This is a helper script that uses ~/.aws/credentials and ~/.aws/config
+# to create a set of managed aws resources which provide an environment
+# in which other aws managed resources (EKS, RDS, etc...) can connect
+#
+# aws configuration (credentials and default region) is required to 
+# perform these tasks
+
+set -e -o pipefail
+
+aws_profile=
+
+while (( "$#" )); do
+  if test -z "$2"; then
+    echo "invalid value for $1 option"
+    exit -1
+  fi
+  case "$1" in
+    -p|--profile)
+      aws_profile=$2
+      shift 2
+      ;;
+    -r|--region)
+      AWS_REGION=$2
+      shift 2
+      ;;
+    *) 
+      shift
+      ;;
+  esac
+done
+
+# if aws_profile is not provided, use default
+aws_profile="${aws_profile:-default}"
+
+# if region is not provided, retrieve aws profile region from config
+AWS_REGION="${AWS_REGION:-$(awk '/["$aws_profile"]/ {getline; print $3}' ${HOME}/.aws/config)}"
+
+# retrieve aws profile credentials, save it under 'default' profile, and base64 encode it
+AWS_CREDS_BASE64=$(cat ${HOME}/.aws/credentials | awk '/["$aws_profile"]/ {getline; print $0}' | awk 'NR==1{print "[default]"}1' | base64 | tr -d "\n")
+
+if test -z "$AWS_REGION"; then
+  echo "error retrieving region from aws config"
+  exit -1
+fi
+
+if test -z "$AWS_CREDS_BASE64"; then
+  echo "error reading credentials from aws config"
+  exit -1
+fi
+
+# build the secret and provider objects
+cat provider.yaml | sed \
+  -e "s|((AWS_REGION))|"$AWS_REGION"|g" \
+  -e "s|((AWS_CREDS_BASE64))|"$AWS_CREDS_BASE64"|g"
