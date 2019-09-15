15ce0cf0688b6c7d0f67611ca94c81b6b0a50a60
soorena776 <javad@upbound.io>
2019-09-15 13:41:44 -0700

    adding cleanup, documentation and more robustness to the scripts
    Signed-off-by: soorena776 <javad@upbound.io>

--- cluster/examples/aws-setup-provider/setup.sh
+++ cluster/examples/aws-setup-provider/setup.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 #
 # This is a helper script that uses ~/.aws/credentials and ~/.aws/config
-# to create a set of managed aws resources which provide an environment
-# in which other aws managed resources (EKS, RDS, etc...) can connect
+# to build an aws provider object
 #
-# aws configuration (credentials and default region) is required to 
-# perform these tasks
+# aws configuration (credentials and default region) is required for this
+# script
 
 set -e -o pipefail
+
+# change to script directory
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
 
 aws_profile=
 
@@ -21,27 +23,26 @@ while (( "$#" )); do
       aws_profile=$2
       shift 2
       ;;
-    -r|--region)
-      AWS_REGION=$2
-      shift 2
-      ;;
     *) 
       shift
       ;;
   esac
 done
 
+# make sure kubectl is configured
+kubectl cluster-info > /dev/null || echo "KUBECONFIG is not configured properly"
+
 # if aws_profile is not provided, use default
 aws_profile="${aws_profile:-default}"
 
 # if region is not provided, retrieve aws profile region from config
-AWS_REGION="${AWS_REGION:-$(awk '/["$aws_profile"]/ {getline; print $3}' ${HOME}/.aws/config)}"
+AWS_REGION=$(awk '/["$aws_profile"]/ {getline; print $3}' ${HOME}/.aws/config)
 
 # retrieve aws profile credentials, save it under 'default' profile, and base64 encode it
 AWS_CREDS_BASE64=$(cat ${HOME}/.aws/credentials | awk '/["$aws_profile"]/ {getline; print $0}' | awk 'NR==1{print "[default]"}1' | base64 | tr -d "\n")
 
 if test -z "$AWS_REGION"; then
-  echo "error retrieving region from aws config"
+  echo "error retrieving region from aws config. "
   exit -1
 fi
 
@@ -50,7 +51,8 @@ if test -z "$AWS_CREDS_BASE64"; then
   exit -1
 fi
 
-# build the secret and provider objects
+# build the secret and provider objects, and then apply it
 cat provider.yaml | sed \
   -e "s|((AWS_REGION))|"$AWS_REGION"|g" \
-  -e "s|((AWS_CREDS_BASE64))|"$AWS_CREDS_BASE64"|g"
+  -e "s|((AWS_CREDS_BASE64))|"$AWS_CREDS_BASE64"|g" \
+  | kubectl apply -f -
