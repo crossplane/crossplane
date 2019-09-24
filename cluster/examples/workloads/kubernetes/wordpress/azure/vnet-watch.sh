18bf0cb430c2ebf9dd9de916dbed9a1a18f86361
hasheddan <georgedanielmangum@gmail.com>
2019-09-24 13:28:24 -0500

    azure workload: implement wordpress example
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

---
+++ cluster/examples/workloads/kubernetes/wordpress/azure/vnet-watch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+trap 'exit 1' SIGINT
+
+echo -n "waiting for mysql endpoint..." >&2
+while kubectl -n azure-infra-dev get mysqlservers -o yaml | grep -q  'items: \[\]'; do
+  echo -n "." >&2
+  sleep 5
+done
+echo "done" >&2
+
+export MYSQL_NAME=$(kubectl -n azure-infra-dev get mysqlservers -o=jsonpath='{.items[0].metadata.name}')
+
+sed "s/MYSQL_NAME/$MYSQL_NAME/g" vnet-rule.yaml | kubectl apply -f -
