8166d091acb1d2b0ed2dca36d03b77dd008bc198
hasheddan <georgedanielmangum@gmail.com>
2020-01-01 23:07:16 -0600

    copyright: bump from 2019 to 2020
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

--- apis/generate.go
+++ apis/generate.go
@@ -1,7 +1,7 @@
 // +build generate
 
 /*
-Copyright 2019 The Crossplane Authors.
+Copyright 2020 The Crossplane Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
