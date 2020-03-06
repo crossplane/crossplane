0316e88f964967507e76172e4c675c6ea8465421
suskin <813571+suskin@users.noreply.github.com>
2020-03-05 19:18:43 -0800

    stack manager: inject stack controller image if not specified
    We want to be able to inject the stack controller image if it isn't
    specified, and we use the image from the stack envelope. This is because
    a common pattern is to use the same image for both the envelope and the
    controller, and if you need to specify the controller image with an
    explicit version tag from inside the envelope image (which is itself),
    it is inconvenient to manage the versions. It's much simpler to be able
    to "inherit" the image and version from whatever was specified on the
    stack install object.
    
    Signed-off-by: suskin <813571+suskin@users.noreply.github.com>

--- pkg/controller/stacks/install/installjob.go
+++ pkg/controller/stacks/install/installjob.go
@@ -247,7 +247,17 @@ func (jc *stackInstallJobCompleter) createJobOutputObject(ctx context.Context, o
 			obj.SetNamespace(ns)
 		}
 
+		// The package isn't *quite* what we will use as the image
+		pkg := i.GetPackage()
+		stackImg, err := i.ImageWithSource(pkg)
+		if err != nil {
+			// Applying the source is best-effort
+			jc.log.Debug("not applying stack envelope image source to stack controller image due to error", "pkg", pkg, "err", err)
+			stackImg = pkg
+		}
+
 		modifiers := []stackSpecModifier{
+			controllerImageInjector(stackImg),
 			controllerPullSetter(i.GetImagePullPolicy(), i.GetImagePullSecrets()),
 			controllerImageSourcer(i),
 			saAnnotationSetter(i.GetServiceAccountAnnotations()),
@@ -425,6 +435,34 @@ func controllerImageSourcer(src imageWithSourcer) stackSpecModifier {
 				cs[i].Image = img
 			}
 		}
+		return nil
+	}
+}
+
+// controllerImageInjector adds a stack image to a stack or stack definition
+// spec, if there isn't one specified. The reason this exists is so that a stack
+// author can use the same image for their stack envelope (the image which is unpacked)
+// and their stack controller (the image which runs in the cluster), because that
+// is a common pattern, and it's inconvenient to manage the image names separately
+// if there are two sources of truth instead of a single source of truth.
+func controllerImageInjector(stackImage string) stackSpecModifier {
+	return func(spec *v1alpha1.StackSpec) error {
+		if d := spec.Controller.Deployment; d != nil {
+			spec := &d.Spec.Template.Spec
+
+			for i := range spec.InitContainers {
+				if spec.InitContainers[i].Image == "" {
+					spec.InitContainers[i].Image = stackImage
+				}
+			}
+
+			for i := range spec.Containers {
+				if spec.Containers[i].Image == "" {
+					spec.Containers[i].Image = stackImage
+				}
+			}
+		}
+
 		return nil
 	}
 }
