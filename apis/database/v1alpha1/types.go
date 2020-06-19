12d7a2165a1cc34317dd09a0bb3e82d756601b16
zzxwill <zzxwill@gmail.com>
2020-06-20 00:35:07 +0800

    Remove description for `allowed engine version`
    Signed-off-by: zzxwill <zzxwill@gmail.com>

--- apis/database/v1alpha1/types.go
+++ apis/database/v1alpha1/types.go
@@ -66,7 +66,7 @@ type MySQLInstanceList struct {
 type PostgreSQLInstanceSpec struct {
 	runtimev1alpha1.ResourceClaimSpec `json:",inline"`
 
-	// EngineVersion specifies the desired PostgreSQL engine version. Allowed Versions: 9.6 and 11.
+	// EngineVersion specifies the desired PostgreSQL engine version.
 	EngineVersion string `json:"engineVersion,omitempty"`
 }
 
