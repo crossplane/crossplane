1a8eb3232327774dd5e44583c4d1b4add1e2ffc7
Muvaffak Onus <onus.muvaffak@gmail.com>
2020-10-02 16:46:03 +0300

    crank: add provider installation command
    Signed-off-by: Muvaffak Onus <onus.muvaffak@gmail.com>

--- cmd/crank/provider/provider.go
+++ cmd/crank/provider/provider.go
@@ -18,20 +18,26 @@ package provider
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/docker/docker/client"
+	"github.com/pkg/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
 
+	"github.com/crossplane/crossplane/apis/pkg/v1alpha1"
 	"github.com/crossplane/crossplane/cmd/crank/pkg"
+	typedclient "github.com/crossplane/crossplane/pkg/client/clientset/versioned/typed/pkg/v1alpha1"
 	"github.com/crossplane/crossplane/pkg/controller/pkg/revision"
 )
 
 // Cmd is the root command for Providers.
 type Cmd struct {
-	Build  BuildCmd    `cmd:"" help:"Build a Provider Package."`
-	Lint   LintCmd     `cmd:"" help:"Lint the contents of a Provider Package."`
-	Push   pkg.PushCmd `cmd:"" help:"Push a Provider Package."`
-	Create CreateCmd   `cmd:"" help:"Create a Provider."`
-	Get    GetCmd      `cmd:"" help:"Get installed Providers."`
+	Build   BuildCmd    `cmd:"" help:"Build a Provider Package."`
+	Lint    LintCmd     `cmd:"" help:"Lint the contents of a Provider Package."`
+	Push    pkg.PushCmd `cmd:"" help:"Push a Provider Package."`
+	Install InstallCmd  `cmd:"" help:"Create a Provider."`
+	Get     GetCmd      `cmd:"" help:"Get installed Providers."`
 }
 
 // Run runs the Provider command.
@@ -92,17 +98,41 @@ func (pc *LintCmd) Run() error {
 	return pc.LintCmd.Lint(ctx, docker, linter)
 }
 
-// CreateCmd creates a Provider in the cluster.
-type CreateCmd struct {
+// InstallCmd creates a Provider in the cluster.
+type InstallCmd struct {
 	Name    string `arg:"" name:"name" help:"Name of Provider."`
 	Package string `arg:"" name:"package" help:"Image containing Provider package."`
 
-	History  int  `help:"Revision history limit."`
-	Activate bool `short:"a" help:"Enable automatic revision activation policy."`
+	RevisionHistoryLimit int64 `help:"Revision history limit."`
+	ManualActivation     bool  `help:"Enable manual revision activation policy."`
 }
 
-// Run the CreateCmd.
-func (p *CreateCmd) Run() error {
+// Run the InstallCmd.
+func (p *InstallCmd) Run() error {
+	ctx := context.TODO()
+	rap := v1alpha1.AutomaticActivation
+	if p.ManualActivation {
+		rap = v1alpha1.ManualActivation
+	}
+	cr := &v1alpha1.Provider{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: p.Name,
+		},
+		Spec: v1alpha1.ProviderSpec{
+			PackageSpec: v1alpha1.PackageSpec{
+				Package:                  p.Package,
+				RevisionActivationPolicy: &rap,
+				RevisionHistoryLimit:     &p.RevisionHistoryLimit,
+			},
+		},
+	}
+	kube := typedclient.NewForConfigOrDie(ctrl.GetConfigOrDie())
+	res, err := kube.Providers().Create(ctx, cr, metav1.CreateOptions{})
+	if err != nil {
+		return errors.Wrap(err, "cannot create provider")
+	}
+	fmt.Printf("%s/%s is created\n", v1alpha1.ProviderGroupKind, res.GetName())
+	// TODO(muvaf): Show nice icons and block until installation completes?
 	return nil
 }
 
