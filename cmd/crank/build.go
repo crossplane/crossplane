900b1d7c33817dd248a47b8bb1e756eb4fd4428d
hasheddan <georgedanielmangum@gmail.com>
2020-10-05 21:35:45 -0500

    Wrap build errors and pass fs in buildChild
    Gives more informative errors and allows for overriding the filesystem
    used for retrieving and writing build artifacts.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

--- cmd/crank/build.go
+++ cmd/crank/build.go
@@ -18,15 +18,22 @@ package main
 
 import (
 	"context"
-	"os"
 	"path/filepath"
 
 	"github.com/google/go-containerregistry/pkg/v1/tarball"
+	"github.com/pkg/errors"
 	"github.com/spf13/afero"
 
 	"github.com/crossplane/crossplane-runtime/pkg/parser"
 
 	"github.com/crossplane/crossplane/pkg/xpkg"
+)
+
+const (
+	errGetNameFromMeta = "failed to get name from crossplane.yaml"
+	errBuildPackage    = "failed to build package"
+	errImageDigest     = "failed to get package digest"
+	errCreatePackage   = "failed to create package file"
 )
 
 // buildCmd builds a package.
@@ -35,42 +42,49 @@ type buildCmd struct {
 	Provider      buildProviderCmd `cmd:"" help:"Build a Provider package."`
 
 	PackageRoot string   `short:"f" help:"Path to crossplane.yaml" default:"."`
-	Ignore      []string `name:"ignore" help:"Paths, specified relative to --package-root, to exclude from the package."`
+	Ignore      []string `help:"Paths, specified relative to --package-root, to exclude from the package."`
 }
 
 // Run runs the build cmd.
-func (c *buildCmd) Run(child *childArg) error {
+func (c *buildCmd) Run(child *buildChild) error {
 	root, err := filepath.Abs(c.PackageRoot)
 	if err != nil {
 		return err
 	}
 
-	fs := afero.NewOsFs()
-
-	pkgName := child.strVal
-	// Extract name if it is not provided.
+	pkgName := child.name
 	if pkgName == "" {
 		metaPath := filepath.Join(root, xpkg.MetaFile)
-		pkgName, err = xpkg.ParseNameFromMeta(fs, metaPath)
+		pkgName, err = xpkg.ParseNameFromMeta(child.fs, metaPath)
 		if err != nil {
-			return err
+			return errors.Wrap(err, errGetNameFromMeta)
 		}
 	}
 
-	be := parser.NewFsBackend(fs, parser.FsDir(root), parser.FsFilters(buildFilters(c.Ignore)...))
-	img, err := xpkg.Build(context.Background(), be)
+	metaScheme, err := xpkg.BuildMetaScheme()
 	if err != nil {
-		return err
+		return errors.New("cannot build meta scheme for package parser")
+	}
+	objScheme, err := xpkg.BuildObjectScheme()
+	if err != nil {
+		return errors.New("cannot build object scheme for package parser")
+	}
+	img, err := xpkg.Build(context.Background(),
+		parser.NewFsBackend(child.fs, parser.FsDir(root), parser.FsFilters(buildFilters(c.Ignore)...)),
+		parser.New(metaScheme, objScheme),
+		child.linter)
+	if err != nil {
+		return errors.Wrap(err, errBuildPackage)
 	}
 
 	hash, err := img.Digest()
 	if err != nil {
-		return err
+		return errors.Wrap(err, errImageDigest)
 	}
 
-	f, err := os.Create(xpkg.BuildPath(root, xpkg.FriendlyID(pkgName, hash.Hex)))
+	f, err := child.fs.Create(xpkg.BuildPath(root, xpkg.FriendlyID(pkgName, hash.Hex)))
 	if err != nil {
-		return err
+		return errors.Wrap(err, errCreatePackage)
 	}
 	defer func() { _ = f.Close() }()
 	return tarball.Write(nil, img, f)
@@ -89,9 +103,15 @@ func buildFilters(skips []string) []parser.FilterFn {
 	return opts
 }
 
+type buildChild struct {
+	name   string
+	linter parser.Linter
+	fs     afero.Fs
+}
+
 // buildConfigCmd builds a Configuration.
 type buildConfigCmd struct {
-	Name strChild `optional:"" help:"Name of the package to be built. Uses name in crossplane.yaml if not specified. Does not correspond to package tag."`
+	Name string `optional:"" help:"Name of the package to be built. Uses name in crossplane.yaml if not specified. Does not correspond to package tag."`
 }
 
 // Run runs the Configuration build cmd.
@@ -99,12 +119,26 @@ func (c *buildConfigCmd) Run() error {
 	return nil
 }
 
+// AfterApply sets the name and linter for the parent build command.
+func (c buildConfigCmd) AfterApply(b *buildChild) error { // nolint:unparam
+	b.name = c.Name
+	b.linter = xpkg.NewConfigurationLinter()
+	return nil
+}
+
 // buildProviderCmd builds a Provider.
 type buildProviderCmd struct {
-	Name strChild `optional:"" help:"Name of the package to be built. Uses name in crossplane.yaml if not specified. Does not correspond to package tag."`
+	Name string `optional:"" help:"Name of the package to be built. Uses name in crossplane.yaml if not specified. Does not correspond to package tag."`
 }
 
 // Run runs the Provider build cmd.
 func (c *buildProviderCmd) Run() error {
+	return nil
+}
+
+// AfterApply sets the name and linter for the parent build command.
+func (c buildProviderCmd) AfterApply(b *buildChild) error { // nolint:unparam
+	b.name = c.Name
+	b.linter = xpkg.NewProviderLinter()
 	return nil
 }
