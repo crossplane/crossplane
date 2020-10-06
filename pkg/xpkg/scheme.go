9137c3b9bb54f18ef824fbdbe15bb89364cbff8f
hasheddan <georgedanielmangum@gmail.com>
2020-10-05 21:47:12 -0500

    Move parser scheme building helpers to xpkg
    The same meta and object schemes are almost always used for building
    crossplane packages, so they have been moved to public functions in the
    xpkg pkg.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

---
+++ pkg/xpkg/scheme.go
@@ -0,0 +1,48 @@
+/*
+Copyright 2020 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package xpkg
+
+import (
+	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	apiextensionsv1alpha1 "github.com/crossplane/crossplane/apis/apiextensions/v1alpha1"
+	pkgmeta "github.com/crossplane/crossplane/apis/pkg/meta/v1alpha1"
+)
+
+// BuildMetaScheme builds the default scheme used for identifying metadata in a
+// Crossplane package.
+func BuildMetaScheme() (*runtime.Scheme, error) {
+	metaScheme := runtime.NewScheme()
+	if err := pkgmeta.SchemeBuilder.AddToScheme(metaScheme); err != nil {
+		return nil, err
+	}
+	return metaScheme, nil
+}
+
+// BuildObjectScheme builds the default scheme used for identifying objects in a
+// Crossplane package.
+func BuildObjectScheme() (*runtime.Scheme, error) {
+	objScheme := runtime.NewScheme()
+	if err := apiextensionsv1alpha1.AddToScheme(objScheme); err != nil {
+		return nil, err
+	}
+	if err := v1beta1.AddToScheme(objScheme); err != nil {
+		return nil, err
+	}
+	return objScheme, nil
+}
