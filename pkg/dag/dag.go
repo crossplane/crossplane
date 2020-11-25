4c8f981086143f0b62aeb6119de39f4bbbc026c4
hasheddan <georgedanielmangum@gmail.com>
2020-11-25 12:47:40 -0600

    Add DAG package
    Adds a package for building and performing operations on a directed
    acyclic graph. The primary implementation uses a map for the underlying
    data structure and is unique in that an edge to a node that does not
    currently exist in the graph will imply the node's existence.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

---
+++ pkg/dag/dag.go
@@ -0,0 +1,238 @@
+/*
+Copyright 2020 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dag
+
+import (
+	"github.com/pkg/errors"
+)
+
+// Node is a node in DAG.
+type Node interface {
+	Identifier() string
+	Neighbors() []Node
+
+	// Depending on how the node is implemented, it may be necessary to check
+	// against duplicates.
+	AddNeighbors(...Node) error
+}
+
+// DAG is a Directed Acyclic Graph.
+type DAG interface {
+	Init(nodes []Node, fns ...NodeFn) ([]Node, error)
+	AddNode(Node) error
+	AddNodes(...Node) error
+	AddOrUpdateNodes(...Node)
+	GetNode(identifier string) (Node, error)
+	AddEdge(from string, to Node) (bool, error)
+	AddEdges(edges map[string][]Node) ([]Node, error)
+	NodeExists(identifier string) bool
+	NodeNeighbors(identifier string) ([]Node, error)
+	TraceNode(identifier string, tree map[string]Node) error
+	Sort() ([]string, error)
+}
+
+// MapDag is a directed acyclic graph implementation that uses a map for its
+// underlying data structure.
+type MapDag struct {
+	nodes map[string]Node
+}
+
+// NodeFn performs executes a function on each node.
+type NodeFn func(int, Node)
+
+// FindIndex searches for the index of a specific node.
+func FindIndex(identifier string, index *int) NodeFn {
+	return func(i int, n Node) {
+		if n.Identifier() == identifier {
+			*index = i
+		}
+	}
+}
+
+// NewDAGFn is a function that returns a DAG.
+type NewDAGFn func() DAG
+
+// NewMapDag creates a new MapDag.
+func NewMapDag() DAG {
+	return &MapDag{}
+}
+
+// Init initializes a MapDag and implies missing destination nodes. Any implied
+// nodes are returned. Any existing nodes are cleared.
+func (d *MapDag) Init(nodes []Node, fns ...NodeFn) ([]Node, error) {
+	d.nodes = map[string]Node{}
+	// Add all nodes before adding edges so we know what nodes were implied.
+	for i, node := range nodes {
+		if err := d.AddNode(node); err != nil {
+			return nil, err
+		}
+		for _, f := range fns {
+			f(i, node)
+		}
+	}
+	var implied []Node // nolint:prealloc
+	for _, node := range nodes {
+		miss, err := d.AddEdges(map[string][]Node{
+			node.Identifier(): node.Neighbors(),
+		})
+		if err != nil {
+			return nil, err
+		}
+		implied = append(implied, miss...)
+	}
+	return implied, nil
+}
+
+// AddNodes adds nodes to the graph.
+func (d *MapDag) AddNodes(nodes ...Node) error {
+	for _, n := range nodes {
+		if err := d.AddNode(n); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// AddNode adds a node to the graph.
+func (d *MapDag) AddNode(node Node) error {
+	if _, ok := d.nodes[node.Identifier()]; ok {
+		return errors.New("node already exists")
+	}
+	d.nodes[node.Identifier()] = node
+	return nil
+}
+
+// AddOrUpdateNodes adds new nodes or updates the existing ones with the same
+// identifier.
+func (d *MapDag) AddOrUpdateNodes(nodes ...Node) {
+	for _, node := range nodes {
+		d.nodes[node.Identifier()] = node
+	}
+}
+
+// NodeExists checks whether a node exists.
+func (d *MapDag) NodeExists(identifier string) bool {
+	if _, ok := d.nodes[identifier]; !ok {
+		return false
+	}
+	return true
+}
+
+// NodeNeighbors returns a node's neighbors.
+func (d *MapDag) NodeNeighbors(identifier string) ([]Node, error) {
+	if _, ok := d.nodes[identifier]; !ok {
+		return nil, errors.New("node does not exist")
+	}
+	return d.nodes[identifier].Neighbors(), nil
+}
+
+// TraceNode returns a node's neighbors and all transitive neighbors using depth
+// first search.
+func (d *MapDag) TraceNode(identifier string, tree map[string]Node) error {
+	if d.nodes[identifier] == nil {
+		return errors.New("missing node in tree")
+	}
+	for _, n := range d.nodes[identifier].Neighbors() {
+		// if we have already visited this neighbor, then we have already
+		// visited its neighbors, so we can skip.
+		if _, ok := tree[n.Identifier()]; ok {
+			continue
+		}
+		tree[n.Identifier()] = n
+		if err := d.TraceNode(n.Identifier(), tree); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// GetNode returns a node in the dag.
+func (d *MapDag) GetNode(identifier string) (Node, error) {
+	if _, ok := d.nodes[identifier]; !ok {
+		return nil, errors.Errorf("node %s does not exist", identifier)
+	}
+	return d.nodes[identifier], nil
+}
+
+// AddEdges adds edges to the graph.
+func (d *MapDag) AddEdges(edges map[string][]Node) ([]Node, error) {
+	var missing []Node
+	for f, ne := range edges {
+		for _, e := range ne {
+			implied, err := d.AddEdge(f, e)
+			if implied {
+				missing = append(missing, e)
+			}
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	return missing, nil
+}
+
+// AddEdge adds an edge to the graph.
+func (d *MapDag) AddEdge(from string, to Node) (bool, error) {
+	if _, ok := d.nodes[from]; !ok {
+		return false, errors.Errorf("node %s does not exist", to)
+	}
+	implied := false
+	if _, ok := d.nodes[to.Identifier()]; !ok {
+		implied = true
+		if err := d.AddNode(to); err != nil {
+			return implied, err
+		}
+	}
+	return implied, d.nodes[from].AddNeighbors(to)
+}
+
+// Sort performs topological sort on the graph.
+func (d *MapDag) Sort() ([]string, error) {
+	visited := map[string]bool{}
+	results := make([]string, len(d.nodes))
+	for n, node := range d.nodes {
+		if !visited[n] {
+			stack := map[string]bool{}
+			if err := d.visit(n, node.Neighbors(), stack, visited, results); err != nil {
+				return nil, err
+			}
+		}
+	}
+	return results, nil
+}
+
+func (d *MapDag) visit(name string, neighbors []Node, stack map[string]bool, visited map[string]bool, results []string) error {
+	visited[name] = true
+	stack[name] = true
+	for _, n := range neighbors {
+		if !visited[n.Identifier()] {
+			if err := d.visit(n.Identifier(), d.nodes[n.Identifier()].Neighbors(), stack, visited, results); err != nil {
+				return err
+			}
+		} else if stack[n.Identifier()] {
+			return errors.Errorf("detected cycle on: %s", n.Identifier())
+		}
+	}
+	for i, r := range results {
+		if r == "" {
+			results[i] = name
+			break
+		}
+	}
+	stack[name] = false
+	return nil
+}
