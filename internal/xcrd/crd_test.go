b1978ad472c0c3a31fc50430beb6fbfa99d3431a
Kellin McAvoy <kellinmcavoy@gmail.com>
2021-01-26 16:31:28 -0500

    Fix xrd status schema persisting to crds
    Signed-off-by: Kellin McAvoy <kellinmcavoy@gmail.com>

--- internal/xcrd/crd_test.go
+++ internal/xcrd/crd_test.go
@@ -80,7 +80,42 @@ func TestForCompositeResource(t *testing.T) {
 	singular := "coolcomposite"
 	plural := "coolcomposites"
 
-	schema := `{"required":["spec"],"properties":{"spec":{"required":["storageGB","engineVersion"],"properties":{"engineVersion":{"enum":["5.6","5.7"],"type":"string"},"storageGB":{"type":"integer"}},"type":"object"}},"type":"object"}`
+	schema := `
+{
+  "required": [
+    "spec"
+  ],
+  "properties": {
+    "spec": {
+      "required": [
+        "storageGB",
+        "engineVersion"
+      ],
+      "properties": {
+        "engineVersion": {
+          "enum": [
+            "5.6",
+            "5.7"
+          ],
+          "type": "string"
+        },
+        "storageGB": {
+          "type": "integer"
+        }
+      },
+      "type": "object"
+    },
+    "status": {
+      "properties": {
+        "phase": {
+          "type": "string"
+        }
+      },
+      "type": "object"
+    }
+  },
+  "type": "object"
+}`
 
 	d := &v1.CompositeResourceDefinition{
 		ObjectMeta: metav1.ObjectMeta{
@@ -239,6 +274,7 @@ func TestForCompositeResource(t *testing.T) {
 							"status": {
 								Type: "object",
 								Properties: map[string]extv1.JSONSchemaProps{
+									"phase": {Type: "string"},
 
 									// From CompositeResourceStatusProps()
 									"conditions": {
@@ -399,7 +435,39 @@ func TestForCompositeResourceClaim(t *testing.T) {
 	claimSingular := "coolclaim"
 	claimPlural := "coolclaims"
 
-	schema := `{"properties":{"spec":{"required":["storageGB","engineVersion"],"properties":{"engineVersion":{"enum":["5.6","5.7"],"type":"string"},"storageGB":{"type":"integer"}},"type":"object"}},"type":"object"}`
+	schema := `
+{
+	"properties": {
+		"spec": {
+			"required": [
+				"storageGB",
+				"engineVersion"
+			],
+			"properties": {
+				"engineVersion": {
+					"enum": [
+						"5.6",
+						"5.7"
+					],
+					"type": "string"
+				},
+				"storageGB": {
+					"type": "integer"
+				}
+			},
+			"type": "object"
+		},
+		"status": {
+      "properties": {
+        "phase": {
+          "type": "string"
+        }
+      },
+      "type": "object"
+    }
+	},
+	"type": "object"
+}`
 
 	d := &v1.CompositeResourceDefinition{
 		ObjectMeta: metav1.ObjectMeta{
@@ -549,6 +617,7 @@ func TestForCompositeResourceClaim(t *testing.T) {
 								"status": {
 									Type: "object",
 									Properties: map[string]extv1.JSONSchemaProps{
+										"phase": {Type: "string"},
 
 										// From CompositeResourceStatusProps()
 										"conditions": {
