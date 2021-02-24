29c2f3ee2a734550defd5c85d68427d476fe09be
Muvaffak Onus <me@muvaf.com>
2021-02-24 18:04:51 +0300

    initializer: switch to parser backend from crossplane-runtime for reading crd files in the container system
    Signed-off-by: Muvaffak Onus <me@muvaf.com>

--- internal/initializer/crds.go
+++ internal/initializer/crds.go
@@ -17,15 +17,15 @@ limitations under the License.
 package initializer
 
 import (
-	"bufio"
 	"context"
-	"io"
-	"os"
-	"path/filepath"
+
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/crossplane/crossplane-runtime/pkg/parser"
+	"github.com/spf13/afero"
 
 	"github.com/pkg/errors"
-	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	"k8s.io/apimachinery/pkg/util/yaml"
+	extv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/crossplane/crossplane-runtime/pkg/resource"
@@ -43,43 +43,33 @@ type CoreCRDs struct {
 
 // Run applies all CRDs in the given directory.
 func (c *CoreCRDs) Run(ctx context.Context, kube client.Client) error { // nolint:gocyclo
-	var crds []*v1.CustomResourceDefinition
-	err := filepath.Walk(c.Path, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if info.IsDir() {
-			return nil
-		}
-		file, err := os.Open(filepath.Clean(path))
-		if err != nil {
-			return errors.Wrapf(err, "cannot read file %s", path)
-		}
-		yr := yaml.NewYAMLReader(bufio.NewReader(file))
-		for {
-			bytes, err := yr.Read()
-			if err != nil && err != io.EOF {
-				return errors.Wrap(err, "cannot read YAML")
-			}
-			if err == io.EOF {
-				break
-			}
-			if len(bytes) < 5 {
-				continue
-			}
-			crd := &v1.CustomResourceDefinition{}
-			if err := yaml.Unmarshal(bytes, crd); err != nil {
-				return errors.Wrap(err, "cannot unmarshal YAML file into CustomResourceDefinition struct")
-			}
-			crds = append(crds, crd)
-		}
-		return nil
-	})
+	r, err := parser.NewFsBackend(afero.NewOsFs(),
+		parser.FsDir(c.Path),
+		parser.FsFilters(
+			parser.SkipDirs(),
+			parser.SkipNotYAML(),
+			parser.SkipEmpty(),
+		),
+	).Init(ctx)
 	if err != nil {
-		return errors.Wrap(err, "cannot walk the crds directory")
+		return errors.Wrap(err, "cannot init filesystem")
+	}
+	defer func() { _ = r.Close() }()
+	s := runtime.NewScheme()
+	if err := extv1.AddToScheme(s); err != nil {
+		return errors.Wrap(err, "cannot add crd to scheme")
+	}
+	p := parser.New(nil, s)
+	pkg, err := p.Parse(ctx, r)
+	if err != nil {
+		return errors.Wrap(err, "cannot parse files")
 	}
 	pa := resource.NewAPIPatchingApplicator(kube)
-	for _, crd := range crds {
+	for _, obj := range pkg.GetObjects() {
+		crd, ok := obj.(*extv1.CustomResourceDefinition)
+		if !ok {
+			return errors.New("only crds can exist in initialization directory")
+		}
 		if err := pa.Apply(ctx, crd); err != nil {
 			return errors.Wrap(err, "cannot apply crd")
 		}
