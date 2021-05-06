66d8c6c45b5869f036aa79378713bc1a4d7e77f6
Steven Borrelli <steve@borrelli.org>
2021-05-06 17:48:44 +0000

    add plural to xrd shortNames
    Signed-off-by: Steven Borrelli <steve@borrelli.org>

--- apis/apiextensions/v1/xrd_types.go
+++ apis/apiextensions/v1/xrd_types.go
@@ -162,7 +162,7 @@ type CompositeResourceDefinitionControllerStatus struct {
 // +kubebuilder:printcolumn:name="OFFERED",type="string",JSONPath=".status.conditions[?(@.type=='Offered')].status"
 // +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
 // +kubebuilder:subresource:status
-// +kubebuilder:resource:scope=Cluster,categories=crossplane,shortName=xrd
+// +kubebuilder:resource:scope=Cluster,categories=crossplane,shortName=xrd;xrds
 type CompositeResourceDefinition struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
