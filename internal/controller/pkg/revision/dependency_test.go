4ed38f84ef6af6270683169582023dbe0d15a06f
hasheddan <georgedanielmangum@gmail.com>
2021-05-24 13:21:40 -0500

    Update dependency resolution tests to consider if lock does not exist
    In scenarios where the lock does not exist the revision should attempt
    to create it unless it is inactive.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>
    (cherry picked from commit fa8d6f3c39ba991a6c0fbc1af2ef94648b17b3db)

--- internal/controller/pkg/revision/dependency_test.go
+++ internal/controller/pkg/revision/dependency_test.go
@@ -22,7 +22,9 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/pkg/errors"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -79,7 +81,44 @@ func TestResolve(t *testing.T) {
 				meta: &pkgmetav1.Configuration{},
 			},
 			want: want{
-				err: errors.Wrap(errBoom, errGetLock),
+				err: errors.Wrap(errBoom, errGetOrCreateLock),
+			},
+		},
+		"ErrCreateLock": {
+			reason: "Should return error if we cannot get or create lock.",
+			args: args{
+				dep: &PackageDependencyManager{
+					client: &test.MockClient{
+						MockGet:    test.NewMockGetFn(kerrors.NewNotFound(schema.GroupResource{}, "")),
+						MockCreate: test.NewMockCreateFn(errBoom),
+					},
+				},
+				meta: &pkgmetav1.Configuration{},
+				pr: &v1.ConfigurationRevision{
+					Spec: v1.PackageRevisionSpec{
+						DesiredState: v1.PackageRevisionActive,
+					},
+				},
+			},
+			want: want{
+				err: errors.Wrap(errBoom, errGetOrCreateLock),
+			},
+		},
+		"SuccessfulInactiveNoLock": {
+			reason: "Should not return error if we are inactive and lock does not exist.",
+			args: args{
+				dep: &PackageDependencyManager{
+					client: &test.MockClient{
+						MockGet:    test.NewMockGetFn(kerrors.NewNotFound(schema.GroupResource{}, "")),
+						MockCreate: test.NewMockCreateFn(errBoom),
+					},
+				},
+				meta: &pkgmetav1.Configuration{},
+				pr: &v1.ConfigurationRevision{
+					Spec: v1.PackageRevisionSpec{
+						DesiredState: v1.PackageRevisionInactive,
+					},
+				},
 			},
 		},
 		"ErrBuildDag": {
@@ -87,7 +126,8 @@ func TestResolve(t *testing.T) {
 			args: args{
 				dep: &PackageDependencyManager{
 					client: &test.MockClient{
-						MockGet: test.NewMockGetFn(nil),
+						MockGet:    test.NewMockGetFn(kerrors.NewNotFound(schema.GroupResource{}, "")),
+						MockCreate: test.NewMockCreateFn(nil),
 					},
 					newDag: func() dag.DAG {
 						return &dagfake.MockDag{
@@ -113,7 +153,8 @@ func TestResolve(t *testing.T) {
 			args: args{
 				dep: &PackageDependencyManager{
 					client: &test.MockClient{
-						MockGet: test.NewMockGetFn(nil),
+						MockGet:    test.NewMockGetFn(kerrors.NewNotFound(schema.GroupResource{}, "")),
+						MockCreate: test.NewMockCreateFn(nil),
 					},
 					newDag: func() dag.DAG {
 						return &dagfake.MockDag{
