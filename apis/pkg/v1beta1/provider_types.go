45798060bb5e62f3105dcee31b3c28ddc88224af
hasheddan <georgedanielmangum@gmail.com>
2021-06-03 10:46:03 -0500

    Deprecate v1beta1 pkg APIs
    Deprecates v1beta1 pkg APIs in favor of their v1 counterparts. The
    v1beta1 APIs are scheduled for removal in Crossplane v1.6.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

--- apis/pkg/v1beta1/provider_types.go
+++ apis/pkg/v1beta1/provider_types.go
@@ -27,6 +27,8 @@ import (
 // +genclient:nonNamespaced
 
 // Provider is the CRD type for a request to add a provider to Crossplane.
+// [DEPRECATED]: Please use the identical v1 API instead. The v1beta1 API is
+// scheduled to be removed in Crossplane v1.6.
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="INSTALLED",type="string",JSONPath=".status.conditions[?(@.type=='Installed')].status"
 // +kubebuilder:printcolumn:name="HEALTHY",type="string",JSONPath=".status.conditions[?(@.type=='Healthy')].status"
@@ -72,6 +74,8 @@ type ProviderList struct {
 // +genclient:nonNamespaced
 
 // A ProviderRevision that has been added to Crossplane.
+// [DEPRECATED]: Please use the identical v1 API instead. The v1beta1 API is
+// scheduled to be removed in Crossplane v1.6.
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="HEALTHY",type="string",JSONPath=".status.conditions[?(@.type=='Healthy')].status"
 // +kubebuilder:printcolumn:name="REVISION",type="string",JSONPath=".spec.revision"
