a36293ef2b9b9cb9d7cc8ff5883a6d18bfcdcbf3
Carl Henrik Lunde <chlunde@ifi.uio.no>
2021-08-06 18:20:16 +0200

    Re-order imports with goimports and add license
    Signed-off-by: Carl Henrik Lunde <chlunde@ifi.uio.no>

--- internal/controller/pkg/revision/deployment_test.go
+++ internal/controller/pkg/revision/deployment_test.go
@@ -1,17 +1,35 @@
+/*
+Copyright 2021 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package revision
 
 import (
 	"testing"
 
-	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
-	pkgmetav1 "github.com/crossplane/crossplane/apis/pkg/meta/v1"
-	v1 "github.com/crossplane/crossplane/apis/pkg/v1"
-	"github.com/crossplane/crossplane/apis/pkg/v1alpha1"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
+
+	pkgmetav1 "github.com/crossplane/crossplane/apis/pkg/meta/v1"
+	v1 "github.com/crossplane/crossplane/apis/pkg/v1"
+	"github.com/crossplane/crossplane/apis/pkg/v1alpha1"
 )
 
 type deploymentModifier func(*appsv1.Deployment)
@@ -22,7 +40,7 @@ func withPodTemplateLabels(labels map[string]string) deploymentModifier {
 	}
 }
 
-var (
+const (
 	namespace = "ns"
 )
 
