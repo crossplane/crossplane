4e26a9dcae8b814abe51991be38cf21ce8bad14e
hasheddan <georgedanielmangum@gmail.com>
2021-10-19 15:28:27 -0400

    Add note clarifying use of package slice index
    Adds a note to clarify why we are foregoing use of the range index in
    favor of the shared packages index.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>
    (cherry picked from commit 12d38f74d0bf615c108507ea26b4e24d566fec2d)

--- internal/initializer/installer.go
+++ internal/initializer/installer.go
@@ -61,7 +61,9 @@ type PackageInstaller struct {
 // Run makes sure all specified packages exist.
 func (pi *PackageInstaller) Run(ctx context.Context, kube client.Client) error {
 	pkgs := make([]client.Object, len(pi.Providers)+len(pi.Configurations))
-	idx := 0
+	// NOTE(hasheddan): we maintain a separate index from the range so that
+	// Providers and Configurations can be added to the same slice for applying.
+	pkgsIdx := 0
 	for _, img := range pi.Providers {
 		p := &v1.Provider{
 			ObjectMeta: metav1.ObjectMeta{
@@ -73,8 +75,8 @@ func (pi *PackageInstaller) Run(ctx context.Context, kube client.Client) error {
 				},
 			},
 		}
-		pkgs[idx] = p
-		idx++
+		pkgs[pkgsIdx] = p
+		pkgsIdx++
 	}
 	for _, img := range pi.Configurations {
 		c := &v1.Configuration{
@@ -87,8 +89,8 @@ func (pi *PackageInstaller) Run(ctx context.Context, kube client.Client) error {
 				},
 			},
 		}
-		pkgs[idx] = c
-		idx++
+		pkgs[pkgsIdx] = c
+		pkgsIdx++
 	}
 	pa := resource.NewAPIPatchingApplicator(kube)
 	for _, p := range pkgs {
