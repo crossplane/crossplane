bf9b1264b9641b91c34f2d715555f5d886603ed1
Nic Cope <negz@rk0n.org>
2022-01-04 01:38:52 +0000

    Remove (most) v1beta1 pkg types
    I believe the only type we haven't deprecated is Lock, which is not yet
    at v1.
    
    Signed-off-by: Nic Cope <negz@rk0n.org>
    (cherry picked from commit b5eea7f02189619c3d5fc8c24f8096ef0e70e34c)

--- internal/client/clientset/versioned/typed/pkg/v1beta1/generated_expansion.go
+++ internal/client/clientset/versioned/typed/pkg/v1beta1/generated_expansion.go
@@ -18,12 +18,4 @@ limitations under the License.
 
 package v1beta1
 
-type ConfigurationExpansion interface{}
-
-type ConfigurationRevisionExpansion interface{}
-
 type LockExpansion interface{}
-
-type ProviderExpansion interface{}
-
-type ProviderRevisionExpansion interface{}
