49b76d78669241e2cb52afe6cc2361f6793ea615
Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>
2022-10-10 14:59:31 +0300

    Update MR API conventions document discussing the pause annotation
    - Update the "Managed Resources" document with information about
      the pause annotation.
    - Update the "Composition" reference documentation with information
      about the pause annotations on Composite Resources and Composite
      Resource Claims.
    
    Signed-off-by: Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>

--- internal/controller/apiextensions/claim/reconciler_test.go
+++ internal/controller/apiextensions/claim/reconciler_test.go
@@ -31,11 +31,10 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
-	"github.com/crossplane/crossplane-runtime/pkg/meta"
-
 	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
 	"github.com/crossplane/crossplane-runtime/pkg/errors"
 	"github.com/crossplane/crossplane-runtime/pkg/logging"
+	"github.com/crossplane/crossplane-runtime/pkg/meta"
 	"github.com/crossplane/crossplane-runtime/pkg/resource"
 	"github.com/crossplane/crossplane-runtime/pkg/resource/fake"
 	"github.com/crossplane/crossplane-runtime/pkg/resource/unstructured/claim"
