8ec6af1bed606267d86decf6d8f735f2f4772e3c
Nic Cope <nicc@rk0n.org>
2022-11-28 15:33:01 -0800

    Remove notices that Composition Revision related fields are alpha
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- internal/xcrd/crd_test.go
+++ internal/xcrd/crd_test.go
@@ -247,12 +247,10 @@ func TestForCompositeResource(t *testing.T) {
 										Properties: map[string]extv1.JSONSchemaProps{
 											"name": {Type: "string"},
 										},
-										Description: "Alpha: This field may be deprecated or changed without notice.",
 									},
 									"compositionRevisionSelector": {
-										Description: "Alpha: This field may be deprecated or changed without notice.",
-										Type:        "object",
-										Required:    []string{"matchLabels"},
+										Type:     "object",
+										Required: []string{"matchLabels"},
 										Properties: map[string]extv1.JSONSchemaProps{
 											"matchLabels": {
 												Type: "object",
@@ -269,8 +267,7 @@ func TestForCompositeResource(t *testing.T) {
 											{Raw: []byte(`"Automatic"`)},
 											{Raw: []byte(`"Manual"`)},
 										},
-										Default:     &extv1.JSON{Raw: []byte(`"Automatic"`)},
-										Description: "Alpha: This field may be deprecated or changed without notice.",
+										Default: &extv1.JSON{Raw: []byte(`"Automatic"`)},
 									},
 									"claimRef": {
 										Type:     "object",
@@ -686,9 +683,8 @@ func TestForCompositeResourceClaim(t *testing.T) {
 											},
 										},
 										"compositionRevisionSelector": {
-											Description: "Alpha: This field may be deprecated or changed without notice.",
-											Type:        "object",
-											Required:    []string{"matchLabels"},
+											Type:     "object",
+											Required: []string{"matchLabels"},
 											Properties: map[string]extv1.JSONSchemaProps{
 												"matchLabels": {
 													Type: "object",
