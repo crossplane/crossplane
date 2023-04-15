1e382dd7d4115879e7604150a6124a436618a1cc
Bob Haddleton <bob.haddleton@nokia.com>
2023-04-15 18:17:16 -0500

    Add shortNames for resources
    Signed-off-by: Bob Haddleton <bob.haddleton@nokia.com>

--- apis/apiextensions/v1/composition_revision_types.go
+++ apis/apiextensions/v1/composition_revision_types.go
@@ -104,7 +104,7 @@ type CompositionRevisionStatus struct {
 // +kubebuilder:printcolumn:name="XR-KIND",type="string",JSONPath=".spec.compositeTypeRef.kind"
 // +kubebuilder:printcolumn:name="XR-APIVERSION",type="string",JSONPath=".spec.compositeTypeRef.apiVersion"
 // +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
-// +kubebuilder:resource:scope=Cluster,categories=crossplane
+// +kubebuilder:resource:scope=Cluster,categories=crossplane,shortName=comprev
 // +kubebuilder:subresource:status
 type CompositionRevision struct {
 	metav1.TypeMeta   `json:",inline"`
