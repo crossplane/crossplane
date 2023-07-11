0702d10e8ec7e49438728f054e6462e9ae4fa267
Dr. Stefan Schimanski <stefan.schimanski@upbound.io>
2023-07-11 17:00:39 +0200

    controller/rbac/definition: edge based events for ClusterRole apply
    Signed-off-by: Dr. Stefan Schimanski <stefan.schimanski@upbound.io>

--- internal/controller/rbac/definition/reconciler.go
+++ internal/controller/rbac/definition/reconciler.go
@@ -19,6 +19,8 @@ package definition
 
 import (
 	"context"
+	"fmt"
+	"sort"
 	"strings"
 	"time"
 
@@ -178,9 +180,10 @@ func (r *Reconciler) Reconcile(ctx context.Context, req reconcile.Request) (reco
 		return reconcile.Result{Requeue: false}, nil
 	}
 
+	applied := make([]string, 0)
 	for _, cr := range r.rbac.RenderClusterRoles(d) {
 		cr := cr // Pin range variable so we can take its address.
-		log = log.WithValues("role-name", cr.GetName())
+		log := log.WithValues("role-name", cr.GetName())
 		err := r.client.Apply(ctx, &cr, resource.MustBeControllableBy(d.GetUID()), resource.AllowUpdateIf(ClusterRolesDiffer))
 		if resource.IsNotAllowed(err) {
 			log.Debug("Skipped no-op RBAC ClusterRole apply")
@@ -193,11 +196,18 @@ func (r *Reconciler) Reconcile(ctx context.Context, req reconcile.Request) (reco
 			return reconcile.Result{}, err
 		}
 		log.Debug("Applied RBAC ClusterRole")
+		applied = append(applied, cr.GetName())
+	}
+
+	sort.Strings(applied)
+	if len(applied) > 3 {
+		r.record.Event(d, event.Normal(reasonApplyRoles, fmt.Sprintf("Applied RBAC ClusterRoles: %s, %s, %s, and %d more", applied[0], applied[1], applied[2], len(applied)-3)))
+	} else if len(applied) > 0 {
+		r.record.Event(d, event.Normal(reasonApplyRoles, fmt.Sprintf("Applied RBAC ClusterRoles: %s", strings.Join(applied, ", "))))
 	}
 
 	// TODO(negz): Add a condition that indicates the RBAC manager is managing
 	// cluster roles for this XRD?
-	r.record.Event(d, event.Normal(reasonApplyRoles, "Applied RBAC ClusterRoles"))
 
 	// There's no need to requeue explicitly - we're watching all XRDs.
 	return reconcile.Result{Requeue: false}, nil
