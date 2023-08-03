959b1982e793634e06f7ae25a501d1b5b3a89238
Lucas Caparelli <lucas.caparelli@gympass.com>
2023-08-03 10:46:09 -0300

    fix(readinesCheck): return KnownJSONTypeBoolean for MatchTrue/False checks
    Signed-off-by: Lucas Caparelli <lucas.caparelli@gympass.com>
    Signed-off-by: Rohit31201 <rohit31rajput2001@gmail.com>

--- pkg/validation/apiextensions/v1/composition/readinessChecks.go
+++ pkg/validation/apiextensions/v1/composition/readinessChecks.go
@@ -83,7 +83,9 @@ func getReadinessCheckExpectedType(r v1.ReadinessCheck) xpschema.KnownJSONType {
 		matchType = xpschema.KnownJSONTypeString
 	case v1.ReadinessCheckTypeMatchInteger:
 		matchType = xpschema.KnownJSONTypeInteger
-	case v1.ReadinessCheckTypeNone, v1.ReadinessCheckTypeNonEmpty, v1.ReadinessCheckTypeMatchCondition, v1.ReadinessCheckTypeMatchTrue, v1.ReadinessCheckTypeMatchFalse:
+	case v1.ReadinessCheckTypeMatchTrue, v1.ReadinessCheckTypeMatchFalse:
+		matchType = xpschema.KnownJSONTypeBoolean
+	case v1.ReadinessCheckTypeNone, v1.ReadinessCheckTypeNonEmpty, v1.ReadinessCheckTypeMatchCondition:
 	}
 	return matchType
 }
