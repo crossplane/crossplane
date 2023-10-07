816dbb94bb946a23f8b4fea7d982f46dc4df4dd2
jbasement <j.keller@celonis.com>
2023-10-07 16:32:19 +0200

    Rewrite graphviz test
    Signed-off-by: jbasement <j.keller@celonis.com>

--- internal/printer/graphviz_test.go
+++ internal/printer/graphviz_test.go
@@ -5,7 +5,9 @@ import (
 	"os"
 	"testing"
 
+	"github.com/crossplane/crossplane-runtime/pkg/test"
 	"github.com/crossplane/crossplane/internal/k8s"
+	"github.com/google/go-cmp/cmp"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
@@ -134,4 +136,168 @@ func TestSaveGraph(t *testing.T) {
 		t.Errorf("graph.png was not created: %v", err)
 	}
 
+}
+
+func TestSaveGraph2(t *testing.T) {
+	resourceWithChildren := k8s.Resource{
+		Manifest: &unstructured.Unstructured{
+			Object: map[string]interface{}{
+				"apiVersion": "test.cloud/v1alpha1",
+				"kind":       "ObjectStorage",
+				"metadata": map[string]interface{}{
+					"name":      "test-resource",
+					"namespace": "default",
+				},
+				"status": map[string]interface{}{
+					"conditions": []interface{}{
+						map[string]interface{}{
+							"status": "True",
+							"type":   "Synced",
+						},
+						map[string]interface{}{
+							"status": "True",
+							"type":   "Ready",
+						},
+					},
+				},
+			},
+		},
+		Event: "Successfully selected composition",
+		Children: []k8s.Resource{
+			{
+				Manifest: &unstructured.Unstructured{
+					Object: map[string]interface{}{
+						"apiVersion": "test.cloud/v1alpha1",
+						"kind":       "XObjectStorage",
+						"metadata": map[string]interface{}{
+							"name":      "test-resource-cl4tv",
+							"namespace": "default",
+						},
+						"status": map[string]interface{}{
+							"conditions": []interface{}{
+								map[string]interface{}{
+									"status": "True",
+									"type":   "Synced",
+								},
+								map[string]interface{}{
+									"status": "True",
+									"type":   "Ready",
+								},
+							},
+						},
+					},
+				},
+				Children: []k8s.Resource{
+					{
+						Manifest: &unstructured.Unstructured{
+							Object: map[string]interface{}{
+								"apiVersion": "test.cloud/v1alpha1",
+								"kind":       "Bucket",
+								"metadata": map[string]interface{}{
+									"name":      "test-resource-cl4tv-123",
+									"namespace": "default",
+								},
+								"status": map[string]interface{}{
+									"conditions": []interface{}{
+										map[string]interface{}{
+											"status": "True",
+											"type":   "Synced",
+										},
+										map[string]interface{}{
+											"status": "True",
+											"type":   "Ready",
+										},
+									},
+								},
+							},
+						},
+					},
+					{
+						Manifest: &unstructured.Unstructured{
+							Object: map[string]interface{}{
+								"apiVersion": "test.cloud/v1alpha1",
+								"kind":       "User",
+								"metadata": map[string]interface{}{
+									"name":      "test-resource-user-cl4tv",
+									"namespace": "default",
+								},
+								"status": map[string]interface{}{
+									"conditions": []interface{}{
+										map[string]interface{}{
+											"status": "True",
+											"type":   "Synced",
+										},
+										map[string]interface{}{
+											"status": "True",
+											"type":   "Ready",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	type args struct {
+		resource k8s.Resource
+		fields   []string
+		path     string
+	}
+
+	type want struct {
+		fileExists bool
+		err        error
+	}
+
+	cases := map[string]struct {
+		reason string
+		args   args
+		want   want
+	}{
+		// Test valid resource
+		"ResourceWithChildren": {
+			reason: "Should created PNG file containing this structure: ObjectStorage -> XObjectStorage -> [Bucket, User]",
+			args: args{
+				resource: resourceWithChildren,
+				fields:   []string{"parent", "name", "kind", "namespace", "apiversion", "synced", "ready", "message", "event"},
+				path:     "graph.png",
+			},
+			want: want{
+				fileExists: true,
+				err:        nil,
+			},
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+
+			// Remove file in case it already exists
+			os.Remove("graph.png")
+
+			// Create a GraphPrinter with a buffer writer
+			graphPrinter := &GraphPrinter{}
+			err := graphPrinter.SaveGraph(tc.args.resource, tc.args.fields, tc.args.path)
+			// Check error
+			if diff := cmp.Diff(tc.want.err, err, test.EquateErrors()); diff != "" {
+				t.Errorf("%s\nExample(...): -want, +got:\n%s", tc.reason, diff)
+			}
+
+			// Check if png exists
+			exists := true
+			if _, err := os.Stat(tc.args.path); errors.Is(err, os.ErrNotExist) {
+				exists = false
+			}
+
+			// Check if file exists
+			if diff := cmp.Diff(tc.want.fileExists, exists); diff != "" {
+				t.Errorf("%s\nExample(...): -want, +got:\n%s", tc.reason, diff)
+			}
+
+		})
+
+	}
 }
