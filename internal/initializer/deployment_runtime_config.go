2a41bd5fcca23dac1cd5a88d21b41fc7811204a0
Hasan Turken <turkenh@gmail.com>
2023-10-13 12:02:08 +0300

    Create a default runtime config if not exist
    Signed-off-by: Hasan Turken <turkenh@gmail.com>

---
+++ internal/initializer/deployment_runtime_config.go
@@ -0,0 +1,45 @@
+/*
+Copyright 2021 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package initializer
+
+import (
+	"context"
+
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/crossplane/crossplane-runtime/pkg/errors"
+	"github.com/crossplane/crossplane-runtime/pkg/resource"
+
+	"github.com/crossplane/crossplane/apis/pkg/v1beta1"
+)
+
+const (
+	errCreateDefaultRuntimeConfig = "cannot create default runtime config"
+)
+
+// DefaultDeploymentRuntimeConfig creates a "default" DeploymentRuntimeConfig
+// object. It is a no-op if the object already exists.
+func DefaultDeploymentRuntimeConfig(ctx context.Context, kube client.Client) error {
+	rc := &v1beta1.DeploymentRuntimeConfig{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "default",
+		},
+	}
+	return errors.Wrap(resource.Ignore(kerrors.IsAlreadyExists, kube.Create(ctx, rc)), errCreateDefaultRuntimeConfig)
+}
---
+++ internal/initializer/deployment_runtime_config_test.go
@@ -0,0 +1,82 @@
+/*
+Copyright 2021 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package initializer
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/crossplane/crossplane-runtime/pkg/errors"
+	"github.com/crossplane/crossplane-runtime/pkg/test"
+)
+
+func TestDeploymentRuntimeConfigObject(t *testing.T) {
+	type args struct {
+		kube client.Client
+	}
+	type want struct {
+		err error
+	}
+	cases := map[string]struct {
+		args
+		want
+	}{
+		"FailedToCreate": {
+			args: args{
+				kube: &test.MockClient{
+					MockCreate: func(ctx context.Context, obj client.Object, opts ...client.CreateOption) error {
+						return errBoom
+					},
+				},
+			},
+			want: want{
+				err: errors.Wrap(errBoom, errCreateDefaultRuntimeConfig),
+			},
+		},
+		"SuccessCreated": {
+			args: args{
+				kube: &test.MockClient{
+					MockCreate: func(ctx context.Context, obj client.Object, opts ...client.CreateOption) error {
+						return nil
+					},
+				},
+			},
+		},
+		"SuccessAlreadyExists": {
+			args: args{
+				kube: &test.MockClient{
+					MockCreate: func(ctx context.Context, obj client.Object, opts ...client.CreateOption) error {
+						return kerrors.NewAlreadyExists(schema.GroupResource{}, "default")
+					},
+				},
+			},
+		},
+	}
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			err := DefaultDeploymentRuntimeConfig(context.TODO(), tc.args.kube)
+			if diff := cmp.Diff(tc.want.err, err, test.EquateErrors()); diff != "" {
+				t.Errorf("\n%s\nRun(...): -want err, +got err:\n%s", name, diff)
+			}
+		})
+	}
+}
