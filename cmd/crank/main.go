f8f5326bdc2e36cfa02b5cd3fd7a0eebe429045d
Nic Cope <nicc@rk0n.org>
2023-10-17 13:32:32 -0700

    Rename marketplace login to xpkg login
    Ditto logout.
    
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- cmd/crank/main.go
+++ cmd/crank/main.go
@@ -25,7 +25,6 @@ import (
 
 	"github.com/crossplane/crossplane-runtime/pkg/logging"
 
-	"github.com/crossplane/crossplane/cmd/crank/marketplace"
 	"github.com/crossplane/crossplane/cmd/crank/render"
 	"github.com/crossplane/crossplane/cmd/crank/xpkg"
 	"github.com/crossplane/crossplane/internal/version"
@@ -64,8 +63,6 @@ var cli struct {
 	Update  updateCmd  `cmd:"" help:"Update Crossplane packages."`
 
 	XPKG xpkg.Cmd `cmd:"" help:"Crossplane package management."`
-
-	Marketplace marketplace.Cmd `cmd:"" help:"Interact with the Upbound Marketplace."`
 
 	Alpha struct {
 		// Add here alpha subcommands
--- cmd/crank/xpkg/xpkg.go
+++ cmd/crank/xpkg/xpkg.go
@@ -20,8 +20,10 @@ package xpkg
 // Cmd contains commands for interacting with xpkgs.
 // TODO(lsviben) add the rest of the commands from up (batch, xpextract).
 type Cmd struct {
-	Build buildCmd `cmd:"" help:"Build a package, by default from the current directory."`
-	Push  pushCmd  `cmd:"" help:"Push Crossplane packages."`
+	Build  buildCmd  `cmd:"" help:"Build a package, by default from the current directory."`
+	Push   pushCmd   `cmd:"" help:"Push Crossplane packages."`
+	Login  loginCmd  `cmd:"" help:"Login to the default package registry (xpkg.upbound.io)."`
+	Logout logoutCmd `cmd:"" help:"Logout of the default package registry (xpkg.upbound.io)."`
 }
 
 // Help prints out the help for the xpkg command.
