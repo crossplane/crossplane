9343b53ed03f28ee16a61784acbc2c054bb69f9b
Dr. Stefan Schimanski <stefan.schimanski@upbound.io>
2023-10-18 09:57:58 +0200

    e2e: unify list gvk vars
    Signed-off-by: Dr. Stefan Schimanski <stefan.schimanski@upbound.io>

--- test/e2e/realtimecompositions_test.go
+++ test/e2e/realtimecompositions_test.go
@@ -43,15 +43,11 @@ func init() {
 func TestRealtimeCompositions(t *testing.T) {
 	manifests := "test/e2e/manifests/apiextensions/composition/realtime-compositions"
 
-	nopCrossplaneList := composed.NewList(composed.FromReferenceToList(corev1.ObjectReference{
-		APIVersion: "nop.crossplane.io/v1alpha1",
-		Kind:       "NopResource",
-	}))
-	nopList := composed.NewList(composed.FromReferenceToList(corev1.ObjectReference{
+	claimList := composed.NewList(composed.FromReferenceToList(corev1.ObjectReference{
 		APIVersion: "realtime-revision-selection.e2e.crossplane.io/v1alpha1",
 		Kind:       "NopResource",
 	}))
-	xnopList := composed.NewList(composed.FromReferenceToList(corev1.ObjectReference{
+	xrList := composed.NewList(composed.FromReferenceToList(corev1.ObjectReference{
 		APIVersion: "realtime-revision-selection.e2e.crossplane.io/v1alpha1",
 		Kind:       "XNopResource",
 	}))
@@ -76,14 +72,14 @@ func TestRealtimeCompositions(t *testing.T) {
 			)).
 			Assess("CreateClaim", funcs.AllOf(
 				funcs.ApplyResources(FieldManager, manifests, "claim.yaml"),
+				funcs.InBackground(funcs.LogResources(claimList, withTestLabels)),
+				funcs.InBackground(funcs.LogResources(xrList, withTestLabels)),
 				funcs.InBackground(funcs.LogResources(nopList, withTestLabels)),
-				funcs.InBackground(funcs.LogResources(xnopList, withTestLabels)),
-				funcs.InBackground(funcs.LogResources(nopCrossplaneList, withTestLabels)),
 				funcs.ResourcesCreatedWithin(30*time.Second, manifests, "claim.yaml"),
 				funcs.ResourcesHaveConditionWithin(5*time.Minute, manifests, "claim.yaml", xpv1.Available()),
 			)).
 			Assess("UpdateMR", funcs.AllOf(
-				funcs.ListedResourcesModifiedWith(nopCrossplaneList, 1, func(object k8s.Object) {
+				funcs.ListedResourcesModifiedWith(nopList, 1, func(object k8s.Object) {
 					anns := object.GetAnnotations()
 					if anns == nil {
 						anns = make(map[string]string)
@@ -101,7 +97,7 @@ func TestRealtimeCompositions(t *testing.T) {
 				funcs.DeleteResources(manifests, "claim.yaml"),
 				funcs.ResourcesDeletedWithin(2*time.Minute, manifests, "claim.yaml"),
 			)).
-			WithTeardown("DeletePrerequisites", funcs.ResourcesDeletedAfterListedAreGone(3*time.Minute, manifests, "setup/*.yaml", nopCrossplaneList)).
+			WithTeardown("DeletePrerequisites", funcs.ResourcesDeletedAfterListedAreGone(3*time.Minute, manifests, "setup/*.yaml", nopList)).
 			WithTeardown("DisableAlphaRealtimeCompositions", funcs.AllOf(
 				funcs.AsFeaturesFunc(environment.HelmUpgradeCrossplaneToBase()), // Disable our feature flag.
 				funcs.ReadyToTestWithin(1*time.Minute, namespace),
