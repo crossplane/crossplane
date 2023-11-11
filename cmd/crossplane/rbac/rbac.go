a872c034c9018e598dba91d69ce024e36b1c93c7
Nic Cope <nicc@rk0n.org>
2023-11-11 01:09:46 -0800

    Use controller-runtime's pprof server rather than our own
    I believe this implementation is identical:
    
    https://github.com/kubernetes-sigs/controller-runtime/blob/v0.16.3/pkg/manager/internal.go#L295
    
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- cmd/crossplane/rbac/rbac.go
+++ cmd/crossplane/rbac/rbac.go
@@ -18,9 +18,6 @@ limitations under the License.
 package rbac
 
 import (
-	"net/http"
-	"net/http/pprof"
-	"path/filepath"
 	"strings"
 	"time"
 
@@ -45,8 +42,6 @@ const (
 	ManagementPolicyAll   = string(rbaccontroller.ManagementPolicyAll)
 	ManagementPolicyBasic = string(rbaccontroller.ManagementPolicyBasic)
 )
-
-const pprofPath = "/debug/pprof/"
 
 // KongVars represent the kong variables associated with the CLI parser
 // required for the RBAC enum interpolation.
@@ -90,32 +85,6 @@ type startCommand struct {
 // Run the RBAC manager.
 func (c *startCommand) Run(s *runtime.Scheme, log logging.Logger) error {
 	log.Debug("Starting", "policy", c.ManagementPolicy)
-	if c.Profile != "" {
-		// NOTE(negz): These log messages attempt to match those emitted by
-		// controller-runtime's metrics HTTP server when it starts.
-		log.Debug("Profiling server is starting to listen", "addr", c.Profile)
-		go func() {
-
-			// Registering these explicitly ensures they're only served by the
-			// HTTP server we start specifically for profiling.
-			mux := http.NewServeMux()
-			mux.HandleFunc(pprofPath, pprof.Index)
-			mux.HandleFunc(filepath.Join(pprofPath, "cmdline"), pprof.Cmdline)
-			mux.HandleFunc(filepath.Join(pprofPath, "profile"), pprof.Profile)
-			mux.HandleFunc(filepath.Join(pprofPath, "symbol"), pprof.Symbol)
-			mux.HandleFunc(filepath.Join(pprofPath, "trace"), pprof.Trace)
-
-			s := &http.Server{
-				Addr:         c.Profile,
-				ReadTimeout:  2 * time.Minute,
-				WriteTimeout: 2 * time.Minute,
-				Handler:      mux,
-			}
-			log.Debug("Starting server", "type", "pprof", "path", pprofPath, "addr", s.Addr)
-			err := s.ListenAndServe()
-			log.Debug("Profiling server has stopped listening", "error", err)
-		}()
-	}
 
 	cfg, err := ctrl.GetConfig()
 	if err != nil {
@@ -130,6 +99,7 @@ func (c *startCommand) Run(s *runtime.Scheme, log logging.Logger) error {
 		Cache: cache.Options{
 			SyncPeriod: &c.SyncInterval,
 		},
+		PprofBindAddress: c.Profile,
 	})
 	if err != nil {
 		return errors.Wrap(err, "cannot create manager")
