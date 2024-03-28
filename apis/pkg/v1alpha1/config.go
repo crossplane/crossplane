7a44c403afec58b0691ebd435fe91cfee9cbdae8
Pete Lumbis <pete@upbound.io>
2024-03-28 15:40:16 -0400

    remove duplicate link
    Signed-off-by: Pete Lumbis <pete@upbound.io>

--- apis/pkg/v1alpha1/config.go
+++ apis/pkg/v1alpha1/config.go
@@ -184,7 +184,6 @@ type PodObjectMeta struct {
 // Read the
 // [Package Runtime Configuration](https://github.com/crossplane/crossplane/blob/11bbe13ea3604928cc4e24e8d0d18f3f5f7e847c/design/one-pager-package-runtime-config.md)
 // design document for more details.
-// https://github.com/crossplane/crossplane/blob/11bbe13ea3604928cc4e24e8d0d18f3f5f7e847c/design/one-pager-package-runtime-config.md
 // +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
 // +kubebuilder:resource:scope=Cluster
 // +kubebuilder:deprecatedversion:warning="ControllerConfig.pkg.crossplane.io/v1alpha1 is deprecated. Use DeploymentRuntimeConfig from pkg.crossplane.io/v1beta1 instead."
