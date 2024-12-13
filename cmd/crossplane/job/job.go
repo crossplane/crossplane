bda55c6ba8f4a60da161668d43a372009808d16b
Patryk Orwat <patryk.orwat@nokia.com>
2024-12-13 16:44:32 +0100

    added composition revision clearing logic
    sign: Patryk Orwat <patryk.orwat@nokia.com>

--- cmd/crossplane/job/job.go
+++ cmd/crossplane/job/job.go
@@ -22,15 +22,18 @@ import (
 	"fmt"
 	"github.com/crossplane/crossplane-runtime/pkg/errors"
 	"github.com/crossplane/crossplane-runtime/pkg/logging"
-	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	"github.com/crossplane/crossplane-runtime/pkg/resource"
+	crossapiextensionsv1 "github.com/crossplane/crossplane/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sort"
+
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/record"
 	"os"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 type Command struct {
@@ -42,6 +45,12 @@ type startCommand struct {
 }
 
 const removeUnusedCompositionRevisionJob = "removeUnusedCompositionRevision"
+
+const keepTopNItems = 3
+
+var compositionsToKeep = map[string]struct{}{
+	"test": struct{}{},
+}
 
 // Run a Crossplane job.
 func (c *startCommand) Run(s *runtime.Scheme, log logging.Logger) error { //nolint:gocognit // Only slightly over.
@@ -67,23 +76,76 @@ func (c *startCommand) Run(s *runtime.Scheme, log logging.Logger) error { //noli
 	})
 	defer eb.Shutdown()
 
-	// Create a dynamic client
-	dynamicClient, err := dynamic.NewForConfig(cfg)
+	crossplaneSchema := runtime.NewScheme()
+	_ = crossapiextensionsv1.AddToScheme(s)
+
+	crossplaneClient, err := client.New(cfg, client.Options{Scheme: crossplaneSchema})
 	if err != nil {
 		return errors.Wrap(err, "cannot create client")
 	}
 
+	k8sClientset, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		return errors.Wrap(err, "cannot create K8S client set")
+	}
+
 	switch c.Job {
 	case removeUnusedCompositionRevisionJob:
-		//todo: clean composite revisions across namespaces
-		gvr := schema.GroupVersionResource{
-			Group:    "apiextensions.crossplane.io",
-			Version:  "v1",
-			Resource: "compositionrevisions",
-		}
-		list, err := dynamicClient.Resource(gvr).Namespace("default").List(ctx, metav1.ListOptions{})
+		// TODO, move out logic and prepare for adding more jobs, especially clearing other revisions
+		namespaces, err := k8sClientset.CoreV1().Namespaces().List(ctx, metav1.ListOptions{})
 		if err != nil {
-			return errors.Wrap(err, "cannot list ")
+			return errors.Wrap(err, "cannot list namespaces")
+		}
+
+		for _, ns := range namespaces.Items {
+			allRevisions := &crossapiextensionsv1.CompositionRevisionList{}
+			err := crossplaneClient.List(ctx, allRevisions, client.InNamespace(ns.GetName()))
+			if err != nil {
+				return errors.Wrap(err, "cannot list composition revisions")
+			}
+
+			var elements = make(map[string]struct{})
+
+			for _, rev := range allRevisions.Items {
+				elements[rev.Spec.CompositeTypeRef.Kind] = struct{}{}
+			}
+
+			for uniqueKind := range elements {
+				// skip clearing loop for configured items
+				if _, found := compositionsToKeep[uniqueKind]; !found {
+					continue
+				}
+				kindRevisions := &crossapiextensionsv1.CompositionRevisionList{}
+				ml := client.MatchingLabels{}
+				ml[crossapiextensionsv1.LabelCompositionName] = uniqueKind
+
+				if err := crossplaneClient.List(ctx, kindRevisions, ml); err != nil {
+					return errors.Wrap(err, "cannot list composition revisions for a label")
+				}
+
+				sort.Slice(kindRevisions.Items, func(i, j int) bool {
+					// sort in descending mode
+					return kindRevisions.Items[i].Spec.Revision > kindRevisions.Items[j].Spec.Revision
+				})
+
+				var revisionsToKeep = make(map[string]struct{})
+
+				for idx, rev := range kindRevisions.Items {
+					// keep top N recent revisions in local cache to keep
+					if keepTopNItems <= idx+1 {
+						revisionsToKeep[rev.GetName()] = struct{}{}
+					}
+				}
+				for _, rev := range kindRevisions.Items {
+					// remove old revisions that aren't in top N recent revisions
+					if _, found := revisionsToKeep[rev.GetName()]; !found {
+						if err := crossplaneClient.Delete(ctx, &rev); resource.IgnoreNotFound(err) != nil {
+							return errors.Wrap(err, "cannot delete composition revision")
+						}
+					}
+				}
+			}
+
 		}
 	default:
 		log.Info(fmt.Sprintf("Available jobs: %s", removeUnusedCompositionRevisionJob))
