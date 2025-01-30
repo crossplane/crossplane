affba346b26582fa5b647fab8a03d203990a997c
Nic Cope <nicc@rk0n.org>
2025-01-29 17:31:37 -0800

    Remove the ControllerConfig type
    This type has been deprecated and replaced by DeploymentRuntimeConfig
    for a long time. It won't be supported in Crossplane v2.
    
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- apis/pkg/pkg.go
+++ apis/pkg/pkg.go
@@ -21,14 +21,12 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 
 	v1 "github.com/crossplane/crossplane/apis/pkg/v1"
-	v1alpha1 "github.com/crossplane/crossplane/apis/pkg/v1alpha1"
 	v1beta1 "github.com/crossplane/crossplane/apis/pkg/v1beta1"
 )
 
 func init() {
 	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
 	AddToSchemes = append(AddToSchemes,
-		v1alpha1.SchemeBuilder.AddToScheme,
 		v1beta1.SchemeBuilder.AddToScheme,
 		v1.SchemeBuilder.AddToScheme,
 	)
