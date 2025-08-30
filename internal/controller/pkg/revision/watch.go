6ed0ac5bb6ff7ee1f73966959ca0c961bdee813d
Nic Cope <nicc@rk0n.org>
2025-08-29 17:53:30 -0700

    Queue reconciles for packages when ImageConfigs change
    Previously we'd only enqueue reconciles when ImageConfigs will pull
    secrets changed. Now we consider rewrite rules too.
    
    Also, adds a bunch of tests for the ImageConfig watch handlers. They
    previously had none.
    
    Fixes https://github.com/crossplane/crossplane/issues/6763
    
    Signed-off-by: Nic Cope <nicc@rk0n.org>
    (cherry picked from commit 1d75b0ecb8ea473f9dc35fae54a4875c85134f20)

--- internal/controller/pkg/revision/watch.go
+++ internal/controller/pkg/revision/watch.go
@@ -32,6 +32,22 @@ import (
 	"github.com/crossplane/crossplane/v2/apis/pkg/v1beta1"
 )
 
+// hasPullSecret returns true if the ImageConfig has authentication with a pull secret.
+func hasPullSecret(ic *v1beta1.ImageConfig) bool {
+	if ic.Spec.Registry == nil {
+		return false
+	}
+	if ic.Spec.Registry.Authentication == nil {
+		return false
+	}
+	return ic.Spec.Registry.Authentication.PullSecretRef.Name != ""
+}
+
+// hasRewriteRules returns true if the ImageConfig has image rewrite rules.
+func hasRewriteRules(ic *v1beta1.ImageConfig) bool {
+	return ic.Spec.RewriteImage != nil
+}
+
 // EnqueuePackageRevisionsForImageConfig enqueues a reconcile for all package
 // revisions an ImageConfig applies to.
 func EnqueuePackageRevisionsForImageConfig(kube client.Client, l v1.PackageRevisionList, log logging.Logger) handler.EventHandler {
@@ -40,8 +56,8 @@ func EnqueuePackageRevisionsForImageConfig(kube client.Client, l v1.PackageRevis
 		if !ok {
 			return nil
 		}
-		// We only care about ImageConfigs that have a pull secret.
-		if ic.Spec.Registry == nil || ic.Spec.Registry.Authentication == nil || ic.Spec.Registry.Authentication.PullSecretRef.Name == "" {
+		// We only care about ImageConfigs that have a pull secret or rewrite rules.
+		if !hasPullSecret(ic) && !hasRewriteRules(ic) {
 			return nil
 		}
 		// Enqueue all package revisions matching the prefixes in the ImageConfig.
