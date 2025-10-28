d409a64b9940846aaf1f1bec81a4618134ac688b
Jared Watts <jbw976@gmail.com>
2025-10-28 13:39:40 -0700

    fix: update CompositionRevision migrator for v1.20 to upgrade from v1beta1 to current (v1)
    Signed-off-by: Jared Watts <jbw976@gmail.com>

--- cmd/crossplane/core/init.go
+++ cmd/crossplane/core/init.go
@@ -95,7 +95,7 @@ func (c *initCommand) Run(s *runtime.Scheme, log logging.Logger) error {
 	// CRD migrator steps are done after core CRDs are applied/updated, so we
 	// are always migrating to the most current storage version
 	steps = append(steps,
-		initializer.NewCoreCRDsMigrator("compositionrevisions.apiextensions.crossplane.io", "v1alpha1"),
+		initializer.NewCoreCRDsMigrator("compositionrevisions.apiextensions.crossplane.io", "v1beta1"),
 		initializer.NewCoreCRDsMigrator("environmentconfigs.apiextensions.crossplane.io", "v1alpha1"),
 		initializer.NewCoreCRDsMigrator("usages.apiextensions.crossplane.io", "v1beta1"),
 		initializer.NewCoreCRDsMigrator("functions.pkg.crossplane.io", "v1beta1"),
