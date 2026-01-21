1ee0613aca1ee89450f96982d685900a1e4b92d2
Adam Wolfe Gordon <awg@upbound.io>
2026-01-21 10:55:12 -0700

    apis: Move common APIs to a new "core" package
    Get rid of the `common`, `common/v1`, and `common/v2` by moving the types that
    live there into a new `core` package. This gets rid of the confusing import path
    `apis/v2/common/v1` and stops using the generic `common` name, providing a more
    specific place for Crossplane's core APIs.
    
    Signed-off-by: Adam Wolfe Gordon <awg@upbound.io>

--- apis/core/doc.go
+++ apis/core/doc.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 The Crossplane Authors.
+Copyright 2026 The Crossplane Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,11 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package v1
-
-import (
-	"github.com/crossplane/crossplane/apis/v2/common"
-)
-
-// MergeOptions Specifies merge options on a field path.
-type MergeOptions = common.MergeOptions
+// Package core contains common API types used by Crossplane resources.
+//
+// +kubebuilder:object:generate=true
+package core
