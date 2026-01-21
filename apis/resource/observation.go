a8434104aae8fd85c04aa70b6b92e4ecb7253def
Adam Wolfe Gordon <awg@upbound.io>
2026-01-21 10:55:12 -0700

    apis: Move common APIs to a new "resource" package
    Get rid of the `common`, `common/v1`, and `common/v2` by moving the types that
    live there into a new `resource` package. This gets rid of the confusing import
    path `apis/v2/common/v1` and stops using the generic `common` name, providing a
    more specific place for common resource-related APIs.
    
    Signed-off-by: Adam Wolfe Gordon <awg@upbound.io>

--- apis/resource/observation.go
+++ apis/resource/observation.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package common
+package resource
 
 // ObservedStatus contains the recent reconciliation stats.
 type ObservedStatus struct {
